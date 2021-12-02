cf5ba1e224427ea8158eceec8ca7b37b05bde53a
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-02 17:42:27 +0100

    Bonus Push

---
+++ BONUS/js_Bonus/script.js
@@ -0,0 +1,9 @@
+// Dichiarazione delle variabili
+
+let b = document.getElementById('base').value;
+
+let h = document.getElementById('height').value;
+
+let A = b * h;
+
+document.getElementById('area').value = "The Area of the rectangle is" + A;
