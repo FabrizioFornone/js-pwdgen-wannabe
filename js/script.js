450e9888d4f9ef66f0e7b3d504dededbf7fae2e0
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-02 16:24:54 +0100

    Comments added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+// Dichiaro le costanti e le associo a dei prompt a schermo
 const userName = prompt("Inserisci il tuo nome");
 const userLastname = prompt("Inserisci il tuo cognome");
 const userFavcolor = prompt("Inserisci il tuo colore preferito");
 
-let htmlElement = document.getElementById('user-psw');
+// Dichiaro la variabile htmlElement e la associo all'oggeto #user-psw
+
+let htmlElement = document.getElementById("user-psw");
+
+/*  Richiamo in pagina con una stringa di codice la variabile htmlElement che è composta
+ da una concatenazione delle costanti dichiarate */
+
 htmlElement.innerHTML = `${userName}${userLastname}${userFavcolor}21 
 `;
